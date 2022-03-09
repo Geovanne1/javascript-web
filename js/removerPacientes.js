520e9194b83e10458c72a53cdc255bc0f38797a0
Geovanne Rodrigues <geovannecaixeta2017@gmail.com>
2022-03-09 16:18:58 -0300

    atualizado

---
+++ js/removerPacientes.js
@@ -0,0 +1,10 @@
+var tabelaPaciente = document.querySelector("#tabela-pacientes");
+
+tabelaPaciente.addEventListener("dblclick", function (event) {
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
+    
+});
